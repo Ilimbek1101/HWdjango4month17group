236f69573a29066f86b11971cbbc4be5eb199ca9
Ilimbek1101 <103297357+Ilimbek1101@users.noreply.github.com>
2022-07-08 16:02:21 +0600

    HW7 django 4 month

--- main/admin.py
+++ main/admin.py
@@ -1,9 +1,24 @@
 from django.contrib import admin
 from .models import Director, Movie, Review
 
+
 # Register your models here.
+class ReviewAdminInline(admin.StackedInline):
+    model = Review
+    extra = 0
+
+class MovieAdmin(admin.ModelAdmin):
+    model = Movie
+    list_display = 'image_img title director description'.split()
+    search_fields = 'title description'.split()
+    list_filter = 'director'.split()
+    readonly_fields = 'image_img'.split()
+    list_editable = 'director'.split()
+    list_per_page = 4
+    inlines = [ReviewAdminInline]
+
 
 
 admin.site.register(Director)
-admin.site.register(Movie)
+admin.site.register(Movie, MovieAdmin)
 admin.site.register(Review)
--- main/models.py
+++ main/models.py
@@ -1,30 +1,47 @@
 from django.db import models
+from django.utils.safestring import mark_safe
 
 
 # Create your models here.
 
 class Director(models.Model):
-    name = models.CharField(max_length=1000, unique=True)
+    class Meta:
+        verbose_name = 'режиссер'
+        verbose_name_plural = 'режиссеры'
+    name = models.CharField(max_length=1000, unique=True, verbose_name='Имя режиссера')
 
     def __str__(self):
         return self.name
 
 
 class Movie(models.Model):
+    class Meta:
+        verbose_name = 'фильм'
+        verbose_name_plural = 'фильмы'
     director = models.ForeignKey(Director, on_delete=models.CASCADE,
-                                 null=True)
-    title = models.CharField(max_length=1000, unique=True)
-    description = models.TextField(blank=True)
-    image = models.ImageField(upload_to='movies', null=True)
+                                 null=True, verbose_name='Режиссер')
+    title = models.CharField(max_length=1000, unique=True, verbose_name='Название фильма')
+    description = models.TextField(blank=True, verbose_name='Описание фильма')
+    image = models.ImageField(upload_to='movies', null=True, verbose_name='Картинка')
 
     def __str__(self):
         return self.title
 
+    def image_img(self):
+        if self.image:
+            return mark_safe(f'<img src={self.image.url} width=300px')
+        return mark_safe(f'<img src="https://sun9-78.userapi.com/impf/WR3VxYx35R4UBV671xOXrFoa2OHcjTIjxligrA/N19XS6EMXbc.jpg?size=1590x400&quality=95&crop=0,0,1590,400&sign=de1814451347fab3d0115c2d06e1d0dc&type=cover_group" style="width: 300px">')
+
+
+
 
 class Review(models.Model):
+    class Meta:
+        verbose_name = 'отзыв'
+        verbose_name_plural = 'отзывы'
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE,
-                              null=True)
-    text = models.TextField()
+                              null=True, verbose_name='Фильм')
+    text = models.TextField(verbose_name='Текст отзыва')
 
     def __str__(self):
         return self.text
